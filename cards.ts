e77f50b3324f4b1c1af2fa33a0d8ada87d1388ed
ajo-sluz <alessandro_joller@sluz.ch>
2026-05-06 15:15:06 +0200

    etc: work in progress

--- cards.ts
+++ cards.ts
@@ -1,3 +1,57 @@
 export function hello(): string {
   return "world";
 }
+import "./enums.ts";
+
+enum CardColor{
+  Rosen = 0,
+  Schellen,
+  Schilten,
+  Eicheln
+}
+
+enum CardValue{
+  Ass = 0,
+  Koenig,
+  Ober,
+  Under,
+  Banner,
+  Neun,
+  Acht,
+  Sieben,
+  Sechs
+}
+
+class Card {
+
+  Color: CardColor;
+  Value: CardValue;
+
+  constructor(color: CardColor, value: CardValue){
+    this.Color = color;
+    this.Value = value
+  }
+
+  public beats(other: Card): boolean {
+    if(this.Value < other.Value){
+      return true;
+    }
+    else{
+      switch(other.Color){
+        case this.Color:
+          return false;
+        case (CardColor.Schilten || CardColor.Schellen):
+          if(this.Color == CardColor.Eicheln){
+            return true;
+          }
+          else{
+            return false;
+          }
+        case CardColor.Rosen:
+          return true;
+        default:
+          return false;
+      }
+    }
+  }
+}
